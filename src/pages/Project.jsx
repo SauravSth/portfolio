1ff4ef80af04d2e4dd0b4b7d0fb965f85e410a60
Saurav Shrestha <sauravshrestha28@gmail.com>
2024-06-29 23:21:15 -0400

    successful fetch from github

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 export default function Contact() {
-	return <h1>Projects Page</h1>;
+	const [projects, setProjects] = useState([]);
+	useEffect(() => {
+		fetch('https://api.github.com/users/SauravSth/repos')
+			.then((res) => res.json())
+			.then((results) => {
+				const projectCard = results.map((result) => (
+					<h1 key={result.id}>Project Name: {result.name}</h1>
+				));
+				setProjects(projectCard);
+			});
+	}, []);
+	return (
+		<>
+			<h1 className="heading">
+				Here are some of the projects I've done!
+			</h1>
+			{projects}
+		</>
+	);
 }
